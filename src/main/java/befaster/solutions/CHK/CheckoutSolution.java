ec8a289b3465d48cc7089654c6a4ffc7e43b894a
sbx_user1051 <sbx_user1051@169.254.7.165>
2019-12-18 15:32:36 +0000

    Wed Dec 18 15:32:36 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,24 @@
 package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
+import jdk.internal.jline.internal.Preconditions;
 
 public class CheckoutSolution {
+
+    /**
+     * +------+-------+----------------+
+     * | Item | Price | Special offers |
+     * +------+-------+----------------+
+     * | A    | 50    | 3A for 130     |
+     * | B    | 30    | 2B for 45      |
+     * | C    | 20    |                |
+     * | D    | 15    |                |
+     * +------+-------+----------------+
+     * @param skus
+     * @return
+     */
+
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        Preconditions.checkNotNull(skus);
     }
 }
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,5 +1,22 @@
+package befaster.solutions.CHK;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.*;
 
 public class CheckoutSolutionTest {
 
+    private CheckoutSolution chk;
+
+    @Before
+    public void setUp() throws Exception {
+        chk = new CheckoutSolution();
+    }
+
+    @Test
+    public void shouldReturnNegativeOneOnNull() {
+        assertThat(chk.checkout(null), equalTo(-1));
+    }
 }
