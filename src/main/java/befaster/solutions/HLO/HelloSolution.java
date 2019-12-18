bbe1d55d72fae8708bbebe0c74cd2f4a990ad28f
sbx_user1051 <sbx_user1051@169.254.7.165>
2019-12-18 15:19:52 +0000

    Wed Dec 18 15:19:52 UTC 2019

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -1,12 +1,16 @@
 package befaster.solutions.HLO;
 
 import befaster.runner.SolutionNotImplementedException;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
 
 public class HelloSolution {
     public String hello(String friendName) {
-        return "Hello, " + friendName + "!";
+        String name = MoreObjects.firstNonNull(friendName, "World");
+        return "Hello, " + name + "!";
     }
 }
+
 
 
 
