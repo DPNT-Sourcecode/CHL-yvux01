5d7cb51a7cc74068e5c61c934f60f77e0ad5bb3e
sbx_user1051 <sbx_user1051@169.254.7.165>
2019-12-18 16:45:16 +0000

    Wed Dec 18 16:45:16 UTC 2019

--- src/main/java/befaster/solutions/CHL/discounters/DiscounterFacade.java
+++ src/main/java/befaster/solutions/CHL/discounters/DiscounterFacade.java
@@ -1,4 +1,29 @@
 package befaster.solutions.CHL.discounters;
 
-public class DiscounterFacade {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class DiscounterFacade implements Discounter {
+
+    Map<Character, Set<Discounter>> discounters = new HashMap<>();
+
+    public DiscounterFacade() {
+        addDiscounter('A', new ASkuDiscounter())
+    }
+
+    private void addDiscounter(char a, Discounter discounter) {
+        discounters.putIfAbsent(a, new HashSet<>());
+        discounters.computeIfPresent(a, (k, v) -> {
+            v.add(discounter);
+            return v;
+        });
+    }
+
+    @Override
+    public int getDiscount(int value, int noOfitems) {
+
+    }
 }
+
