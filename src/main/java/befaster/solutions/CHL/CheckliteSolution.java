1e97750e4a3965a4880c4884012121ef7c52e2db
sbx_user1051 <sbx_user1051@169.254.7.165>
2019-12-18 16:24:14 +0000

    Wed Dec 18 16:24:14 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -1,9 +1,64 @@
 package befaster.solutions.CHL;
 
 import befaster.runner.SolutionNotImplementedException;
+import com.google.common.collect.ImmutableSet;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class CheckliteSolution {
+
+    public static final Set<Character> validSkus = ImmutableSet.of('A', 'B', 'C', 'D');
+
     public Integer checklite(String skus) {
-        throw new SolutionNotImplementedException();
+        if (skus == null) {
+            return -1;
+        }
+
+        char[] chars = skus.toCharArray();
+
+        if (!isValidSkus(chars)) {
+            return -1;
+        }
+
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char c : chars) {
+            charCount.compute(c, (k, v) -> v == null ? 1 : ++v);
+        }
+
+        Integer dCount = charCount.compute('D', (k, v) -> v == null ? 0 : calculateDTotal(v));
+        Integer cCount = charCount.compute('C', (k, v) -> v == null ? 0 : calculateCTotal(v));
+        Integer bCount = charCount.compute('B', (k, v) -> v == null ? 0 : calculateBTotal(v));
+        Integer aCount = charCount.compute('A', (k, v) -> v == null ? 0 : calculateATotal(v));
+
+
+        return dCount + cCount + bCount + aCount;
+    }
+
+    private boolean isValidSkus(char[] chars) {
+        for (char c : chars) {
+            if (!validSkus.contains(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int calculateDTotal(Integer v) {
+        return v * 15;
+    }
+
+    private int calculateCTotal(Integer v) {
+        return v * 20;
+    }
+
+    private Integer calculateBTotal(Integer v) {
+        return (v / 2 * 45) + (v % 2 * 30);
+    }
+
+    private Integer calculateATotal(Integer v) {
+        return (v / 3 * 130) + (v % 3 * 50);
     }
 }
---
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -0,0 +1,57 @@
+package befaster.solutions.CHL;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
+public class CheckliteSolutionTest {
+
+    private CheckliteSolution chl;
+
+    @Before
+    public void setUp() throws Exception {
+        chl = new CheckliteSolution();
+    }
+
+    @Test
+    public void shouldReturnNegativeOneOnNull() {
+        assertThat(chl.checklite(null), equalTo(-1));
+    }
+
+    @Test
+    public void shouldReturnNegativeOneOnIllegalSku() {
+        assertThat(chl.checklite("E"), equalTo(-1));
+    }
+
+    @Test
+    public void shouldCalculateDSkus() {
+        assertThat(chl.checklite("DD"), equalTo(30));
+    }
+
+    @Test
+    public void shouldCalculateCSkus() {
+        assertThat(chl.checklite("CC"), equalTo(40));
+    }
+
+    @Test
+    public void shouldCalculateBSkus() {
+        assertThat(chl.checklite("BBB"), equalTo(75));
+    }
+
+    @Test
+    public void shouldCalculateASkus() {
+        assertThat(chl.checklite("AAAA"), equalTo(180));
+    }
+
+    @Test
+    public void shouldCalculateAllSkusNoOffers() {
+        assertThat(chl.checklite("CDBA"), equalTo(115));
+    }
+
+    @Test
+    public void shouldCalculateAllSkusWithOffers() {
+        assertThat(chl.checklite("BABAA"), equalTo(175));
+    }
+}
