b0a5366bcb964a7553bbe0d0a3c2ba7b5e535a99
sbx_user1051 <sbx_user1051@169.254.7.165>
2019-12-18 17:03:16 +0000

    Wed Dec 18 17:03:16 UTC 2019

--- src/main/java/befaster/solutions/CHL/discounters/ASkuDiscounter.java
+++ src/main/java/befaster/solutions/CHL/discounters/ASkuDiscounter.java
@@ -3,8 +3,14 @@ package befaster.solutions.CHL.discounters;
 public class ASkuDiscounter implements Discounter {
     @Override
     public int getDiscount(int total, int noOfitems) {
-        return noOfitems / 3 * 20;
+        int validItemsForFirstDiscount = noOfitems / 5;
+        int discountForFive = validItemsForFirstDiscount * 50;
+        int remainingItems = noOfitems % 5;
+        int validItemsForSecondDiscount = remainingItems / 3;
+        int discountForThree = validItemsForSecondDiscount * 20;
+        return discountForFive + discountForThree;
     }
 }
+
 
 
